254b8d5242f1fe09f8d6e7aefc75003c70ffcf7e
i-dont-have-any-good-names <67664504+i-dont-have-any-good-names@users.noreply.github.com>
2022-11-15 06:07:44 +0000

    0.1

--- app.py
+++ app.py
@@ -11,37 +11,37 @@ elif qfa == "cal":
         print('mode: "ma"')
         num1 = int(input("Enter your first number.\n"))
         num2 = int(input("Enter your second number.\n"))
-        sum = num1 + num2
+        numSum = num1 + num2
         print("The sum of this problem is\n" + str(sum))
     elif math == "ms":
         print('mode: "ms"')
         num1 = int(input("Enter your first number.\n"))
         num2 = int(input("Enter your second number.\n"))
-        sum = num1 - num2
+        numSum = num1 - num2
         print("The difference of this problem is\n" + str(sum))
     elif math == "mm":
         print('mode: "mm"')
         num1 = int(input("Enter your first number.\n"))
         num2 = int(input("Enter your second number.\n"))
-        sum = num1 * num2
+        numSum = num1 * num2
         print("The sum of this problem is\n" + str(sum))
     elif math == "me":
         print('mode: "me"')
         num1 = int(input("Enter your first number.\n"))
         num2 = int(input("Enter your second number.\n"))
-        sum = num1 ** num2
+        numSum = num1 ** num2
         print("The sum of this problem is\n" + str(sum))
     elif math == "md":
         print('mode: "math division"')
         num1 = int(input("Enter your first number.\n"))
         num2 = int(input("Enter your second number.\n"))
-        sum = num1 / num2
+        numSum = num1 / num2
         print("The sum of this problem is\n" + str(sum))
     elif math == "mdr":
         print('mode: "math division with remainders"')
         num1 = int(input("Enter your first number.\n"))
         num2 = int(input("Enter your second number.\n"))
-        sum = num1 % num2
+        numSum = num1 % num2
         print("The remainder of this devision problem is\n" + str(sum))
     else:
         print("Enter something valid")
