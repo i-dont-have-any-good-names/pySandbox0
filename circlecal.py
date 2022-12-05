d27ee41cd9fdb6b920ad5bf5adca7c9014b4d55f
i-dont-have-any-good-names <67664504+i-dont-have-any-good-names@users.noreply.github.com>
2022-12-05 04:50:05 +0000

    a

---
+++ circlecal.py
@@ -0,0 +1,13 @@
+pi = 3.14
+user = input()
+def pie(uinput):
+    if user != float(user):
+        print("enter a number")
+    pisum = float(print(pi * uinput))
+    print(pisum)
+    
+
+running = True
+while running == True:
+    pie(user)
+    continue
